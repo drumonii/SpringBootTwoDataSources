cc6ca18469459e31626cb8ff364a8ce95427bd9e
drumonii <drumonii@users.noreply.github.com>
2015-03-05 22:49:49 -0500

    Initial commit
    Running two datasources off of H2

---
+++ src/main/java/demo/config/DataSourceConfig.java
@@ -0,0 +1,52 @@
+package demo.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.stereotype.Controller;
+
+import javax.sql.DataSource;
+
+@Controller
+public class DataSourceConfig {
+
+    private static final String PREFIX_PRIMARY = DataSourceProperties.PREFIX + ".primary";
+    private static final String PREFIX_SECONDARY = DataSourceProperties.PREFIX + ".secondary";
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = PREFIX_PRIMARY)
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = PREFIX_SECONDARY)
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean primaryEntityManagerFactory(EntityManagerFactoryBuilder builder) {
+        return builder
+                .dataSource(primaryDataSource())
+                .packages("demo.model.primary")
+                .persistenceUnit("primary")
+                .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean secondaryEntityManagerFactory(EntityManagerFactoryBuilder builder) {
+        return builder
+                .dataSource(secondaryDataSource())
+                .packages("demo.model.secondary")
+                .persistenceUnit("secondary")
+                .build();
+    }
+
+}
