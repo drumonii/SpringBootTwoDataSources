83b02a14641c39538f711d1f2bca24cc7d128d44
drumonii <drumonii@users.noreply.github.com>
2015-08-09 16:29:21 -0400

    And I did it again... Controller instead of Config

--- src/main/java/demo/config/FlywayConfig.java
+++ src/main/java/demo/config/FlywayConfig.java
@@ -7,10 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Controller;
 
-@Controller
+@Configuration
 public class FlywayConfig {
 
 	@Autowired
