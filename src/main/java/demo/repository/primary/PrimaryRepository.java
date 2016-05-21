00c752451044e1179c7e5f0aa493d475b0df3683
drumonii <drumonii@users.noreply.github.com>
2016-05-21 15:24:50 -0400

    Add Spring Data REST starter. Remove Service layer in favor of SDR

--- src/main/java/demo/repository/primary/PrimaryRepository.java
+++ src/main/java/demo/repository/primary/PrimaryRepository.java
@@ -1,9 +1,11 @@
 package demo.repository.primary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import demo.model.primary.PrimaryModel;
 
+@RepositoryRestResource(path = "primary", collectionResourceRel = "data")
 public interface PrimaryRepository extends JpaRepository<PrimaryModel, Integer>{
 
 }
--- src/main/java/demo/repository/secondary/SecondaryRepository.java
+++ src/main/java/demo/repository/secondary/SecondaryRepository.java
@@ -1,9 +1,11 @@
 package demo.repository.secondary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import demo.model.secondary.SecondaryModel;
 
+@RepositoryRestResource(path = "secondary", collectionResourceRel = "data")
 public interface SecondaryRepository extends JpaRepository<SecondaryModel, Integer> {
 
 }
