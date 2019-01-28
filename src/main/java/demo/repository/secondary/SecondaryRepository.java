2746b12354796379b6184e720200cdea4bed2298
drumonii <drumonii@users.noreply.github.com>
2019-01-27 21:12:11 -0500

    Remove Spring Data REST
    It was over complicating the demo for no real gain.

--- src/main/java/demo/repository/secondary/SecondaryRepository.java
+++ src/main/java/demo/repository/secondary/SecondaryRepository.java
@@ -1,11 +1,9 @@
 package demo.repository.secondary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import demo.model.secondary.SecondaryModel;
 
-@RepositoryRestResource(path = "secondary", collectionResourceRel = "data")
 public interface SecondaryRepository extends JpaRepository<SecondaryModel, Integer> {
 
 	SecondaryModel findByNameIgnoreCase(String name);
--- src/test/java/demo/rest/primary/PrimaryRestControllerTest.java
+++ src/test/java/demo/rest/primary/PrimaryRestControllerTest.java
@@ -3,18 +3,14 @@ package demo.rest.primary;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,23 +20,13 @@ public class PrimaryRestControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Value("${spring.data.rest.base-path}")
-	private String apiPath;
-
 	@Test
 	public void getPrimary() throws Exception {
-		mockMvc.perform(get(apiPath + "/primary"))
+		mockMvc.perform(get("/api/primary"))
 				.andExpect(status().isOk())
-				.andExpect(content().contentType(new MediaType("application", "hal+json", UTF_8)))
-				.andExpect(jsonPath("$._embedded.data").exists())
-				.andExpect(jsonPath("$._links").exists())
-				.andExpect(jsonPath("$._links.self").exists())
-				.andExpect(jsonPath("$._links.self.href").exists())
-				.andExpect(jsonPath("$.page").exists())
-				.andExpect(jsonPath("$.page.size").exists())
-				.andExpect(jsonPath("$.page.totalElements").exists())
-				.andExpect(jsonPath("$.page.totalPages").exists())
-				.andExpect(jsonPath("$.page.number").exists());
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(jsonPath("$.content").isArray())
+				.andExpect(jsonPath("$.pageable").isMap());
 	}
 
 }
