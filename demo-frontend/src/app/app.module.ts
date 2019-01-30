92e36490b5cfb13ee28989bb1f6784e9f30993d3
drumonii <drumonii@users.noreply.github.com>
2019-01-29 22:45:59 -0500

    Add Angular Material

--- demo-frontend/src/app/app.module.ts
+++ demo-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
