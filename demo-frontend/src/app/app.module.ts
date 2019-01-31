656f0af8a37191f2ce3b4b70b5958cd96090ac42
drumonii <drumonii@users.noreply.github.com>
2019-01-30 21:05:50 -0500

    Add HttpClientModule

--- demo-frontend/src/app/app.module.ts
+++ demo-frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { LayoutModule } from './layout/layout.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     LayoutModule
   ],
