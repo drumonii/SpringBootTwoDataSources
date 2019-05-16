e9169918576463d51d16e1e1e5add9e88998aced
drumonii <drumonii@users.noreply.github.com>
2019-05-15 22:45:38 -0400

    PageHeaderComponent's change detection can be OnPush

--- demo-frontend/src/app/components/page-header.component.ts
+++ demo-frontend/src/app/components/page-header.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
-  styleUrls: ['./page-header.component.scss']
+  styleUrls: ['./page-header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PageHeaderComponent implements OnInit {
 
